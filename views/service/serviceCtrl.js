f1e43166d389ff86ff6bba19caf88a5802e842a5
hungNumNum <jimmyhunganh2k@gmail.com>
2023-02-22 22:53:41 +0700

    hung-serviceFE

--- views/service/serviceCtrl.js
+++ views/service/serviceCtrl.js
@@ -1,54 +1,57 @@
-app.controller("serviceCtrl", function ($scope,$http) {
-  $scope.showbut = true;
-  $scope.form={}
-  $scope.items=[];
-  $scope.serTypes = [];
-  $scope.initialize = function(){
+app.controller("serviceCtrl", function ($scope, $http) {
+	$scope.showbut = true;
+	$scope.form = {
+		status:false,
+		serviceType: { id: 1 }
+	};
+	$scope.items = [];
+	$scope.serTypes = [];
+	$scope.initialize = function () {
 
-    $http.get("http://localhost:8000/api/service-rooms").then(resp=>{
+		$http.get("http://localhost:8000/api/service-rooms").then(resp => {
 			$scope.items = resp.data;
 		})
-    
-		$http.get("http://localhost:8000/api/service-types").then(resp=>{
+
+		$http.get("http://localhost:8000/api/service-types").then(resp => {
 			$scope.serTypes = resp.data;
 		})
-		
+
 	}
 	$scope.initialize();
 
-  $scope.create = function(){ //create a new service 
+	$scope.create = function () { //create a new service 
 		var item = angular.copy($scope.form);
-		$http.post("http://localhost:8000/api/service-rooms",item).then(resp=>{
+		$http.post("http://localhost:8000/api/service-rooms", item).then(resp => {
 			alert("Adding success !")
-      $scope.initialize();
-		}).catch(error=>{
+			$scope.initialize();
+		}).catch(error => {
 			alert("Adding failed !")
-			console.log("Error",error)
+			console.log("Error", error)
 		})
-		
+
 	}
 
-	$scope.update = function(){
+	$scope.update = function () {
 		var item = angular.copy($scope.form);
-		$http.put("http://localhost:8000/api/service-rooms",item).then(resp=>{
-			var index = $scope.items.findIndex(p=>p.id==item.id)
+		$http.put("http://localhost:8000/api/service-rooms", item).then(resp => {
+			var index = $scope.items.findIndex(p => p.id == item.id)
 			$scope.items[index] = item;
 			alert("Update success!")
-		}).catch(error=>{
+		}).catch(error => {
 			alert("Update failed!")
-			console.log("Error",error)
+			console.log("Error", error)
 		})
 	}
 
 	$scope.view = function (s) {//update
-			const myModal = new bootstrap.Modal('#exampleModal');
-            myModal.show();
-			$scope.showbut = false;
-			$scope.form=angular.copy(s);
-    }
+		const myModal = new bootstrap.Modal('#exampleModal');
+		myModal.show();
+		$scope.showbut = false;
+		$scope.form = angular.copy(s);
+	}
 	$scope.close = function () {
 		$scope.showbut = true;
-		$scope.form={}
-}
+		$scope.form = {}
+	}
 });
 

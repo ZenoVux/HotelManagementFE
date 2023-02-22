b9e1803cc1713ce66b4038841d60b2c792baa222
vietvp892002 <vietvp892002@gmail.com>
2023-02-22 22:08:53 +0700

    payment_FE

--- router.js
+++ router.js
@@ -11,6 +11,18 @@ app.config(function ($routeProvider) {
       templateUrl: "/views/account/form.html",
       controller: "accountFormCtrl",
     })
+    .when("/payment", {
+      templateUrl: "/views/payment/table.html",
+      controller: "paymentListCtrl",
+    })
+    .when("/payment/create", {
+      templateUrl: "/views/payment/create.html",
+      controller: "paymentCreateCtrl",
+    })
+    .when("/payment/update/:id", {
+      templateUrl: "/views/payment/update.html",
+      controller: "paymentUpdateCtrl",
+    })
     .when("/login", {
       templateUrl: "/views/login.html",
     })

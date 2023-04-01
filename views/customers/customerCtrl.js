d0f1134f658fccd792c94875222a063df5be4e44
tanminh24 <tanminhluong24@gmail.com>
2023-04-01 18:10:57 +0700

    update booking

--- views/customers/customerCtrl.js
+++ views/customers/customerCtrl.js
@@ -1,16 +1,19 @@
 app.controller("customerCtrl", function ($scope, $http) {
     $scope.initTable = function () {
+        $scope.loading = true;
         $http.get("http://localhost:8000/api/customers").then(function (resp) {
             $scope.customers = resp.data;
             $(document).ready(function () {
                 $('#customer-table').DataTable();
             });
+            $scope.loading = false;
         });
     }
     $scope.initTable();
     $scope.viewCustomer = function (customer) {
         $('#customer-modal').modal('show');
         $scope.selectedCustomer = customer;
+        console.log(customer);
     };
 
 });
@@ -18,6 +21,7 @@ app.controller("customerEditCtrl", function ($scope, $routeParams, $http) {
     $scope.getCustomerById = function () {
         $http.get("http://localhost:8000/api/customers/" + $routeParams.customerId).then(function (resp) {
             $scope.customer = resp.data;
+            console.log($scope.customer);
         });
     }
     $scope.getCustomerById();
@@ -33,10 +37,12 @@ app.controller("customerEditCtrl", function ($scope, $routeParams, $http) {
 });
 app.controller("customerTypeCtrl", function ($scope, $http) {
     $scope.getCustomerTypes = function () {
+        $scope.loading = true;
         $http.get("http://localhost:8000/api/customers").then(function (resp) {
             $scope.customers = resp.data;
             $http.get("http://localhost:8000/api/customer-types").then(function (resp) {
                 $scope.customerTypes = resp.data;
+                $scope.loading = false;
                 $scope.customerTypes.forEach(function (customerType) {
                     customerType.numOfCustomers = 0;
                     $scope.customers.forEach(function (customer) {
@@ -44,9 +50,6 @@ app.controller("customerTypeCtrl", function ($scope, $http) {
                             customerType.numOfCustomers++;
                         }
                     });
-                });
-                $(document).ready(function () {
-                    $('#customer-type-table').DataTable();
                 });
             });
         });
